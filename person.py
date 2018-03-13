bdf88931b71669279b7d1931200dabb6ff1a2418
KehanL <37347764+KehanL@users.noreply.github.com>
2018-03-13 16:07:35 -0400

    Add two methods to Person

--- person.py
+++ person.py
@@ -3,11 +3,34 @@
 
 """
 
+
+import datetime
 # The Person class is the parent class of all the people
 class Person:
-    
-    def __init__(self, name):
-        self.name = name
+	
+    #initialize the common attributors that can be inherited
+    def __init__(self, name, age, gender, emailadress, birthdate, phone):
+        self.name = name 
+        self.age = age
+        self.gender = gender
+        self.emailadress = emailadress
+        self.birthdate = birthdate
+        self.phone = phone
+
+    # calculate age by using birthdate and date of today
+    def age(self):
+    	today = datetime.date.today()                    #get date of today
+    	month, day, year = self.birthdate.split("/")     #format the input
+    	age = today.year - int(year)                     #calculate years old
+    	if (today.month, today.day) < (int(month), int(day)):   
+    		age-=1
+
+    	return age
+
+    def email(self):
+    	return ("The request email address is", emailadress)
+
+
 
 class Customer(Person):
     pass
