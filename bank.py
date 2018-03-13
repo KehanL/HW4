a27d1a47f8751624ef7088076fa627c8eadd28d8
俞越 <yuyue@client-130-203-81-32.mobility.psu.edu>
2018-03-13 18:02:46 -0400

    new bank module

---
+++ bank.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Dipicting behavior or the bank
+including ATM, bank accounts, etc.
+
+Todo:
+Account
+"""
+
+# storing account information
+class Account():
+    
+    # initialize
+    def __init__(self, name, balance_saving, balance_checking, account_number):
+        self.name = name
+        self.balance_saving = balance_saving
+        self.balance_checking = balance_checking
+        self.account_number = self.account_number
+    
